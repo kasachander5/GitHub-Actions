471de54ff37e25a404c967cbc6e693ba1f066412
kasachander5 <145572316+kasachander5@users.noreply.github.com>
2024-01-10 22:08:19 +0800

    added image id and region

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "ap-southeast-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0fb653ca2d3203ac1"
+  ami           = "ami-0eeadc4ab092fef70"
   instance_type = "t2.micro"
 
   tags = {
